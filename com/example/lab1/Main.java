0d3fc2bb71320e5f08c62e6a977247f0481720c7
yujin9747 <jyj9747@naver.com>
2021-09-05 13:40:36 +0900

    try2

--- com/example/lab1/Main.java
+++ com/example/lab1/Main.java
@@ -1,9 +1,15 @@
 package com.example.lab1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in) ;
+		System.out.println("Hello World!!!\n") ;
+		System.out.print("이름 입력 : ");
+		String name = s.next();
+		System.out.println("[" + name + "]님 환영합니다~") ;
 
 	}
 
